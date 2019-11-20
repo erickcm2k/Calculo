209328d893d24ca6df60f363b4ca7f9a075085ac
Erick Castañeda Martínez <erickce40@gmail.com>
2019-11-20 02:50:57 -0600

    Add files via upload

---
+++ calculo.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+datos = np.arange(0, 360)
+datos = np.radians(datos)
+datos = 3*np.sin(datos)
+
+plt.plot(datos,"b--")
+plt.show() #función que imprime la gráfica
+#plt.savefig("Gráfica1.png") #función que guarda la gráfica
+
+"""
+Requisitos previos:
+
+Instalar matplotlibt y numpy
+
+sudo apt-get install python-numpy
+sudo apt-get install python-matplotlib
+
+"""
