e5e53a09e7fc2e2999a83892194c818153290030
Erick Castañeda Martínez <erickce40@gmail.com>
2019-11-20 02:59:06 -0600

    .

--- calculo.py
+++ calculo.py
@@ -5,6 +5,6 @@ datos = np.arange(0, 360)
 datos = np.radians(datos)
 datos = 3*np.sin(datos)
 
-plt.plot(datos,"b--")
+plt.plot(datos,"b") #usando b-- se imprime seccionada la gráfica
 plt.show() #función que imprime la gráfica
 #plt.savefig("Gráfica1.png") #función que guarda la gráfica
